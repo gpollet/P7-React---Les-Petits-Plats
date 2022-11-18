8b705133f40a79379ff7c077a2e699bccadc4677
arktal <101146331+gpollet@users.noreply.github.com>
2022-11-18 15:12:19 +0100

    Recipe cards are now links Improved accessibility

--- src/scripts/components/top-filters.js
+++ src/scripts/components/top-filters.js
@@ -4,15 +4,15 @@ export function createRecipeFilters(container) {
 
   container.innerHTML += `<div class="top-filters_container">
 <label>
-<input class="top-filters_ingredients" name="ingredients" placeholder="Ingrédients"></input>${chevronDownIcon}
+<input class="top-filters_ingredients" name="ingredients" placeholder="Ingrédients" aria-label="Rechercher par ingrédients"></input>${chevronDownIcon}
 </label>
 
 <label>
-<input class="top-filters_appareils" name="appareils" placeholder="Appareils"></input>${chevronDownIcon}
+<input class="top-filters_appareils" name="appareils" placeholder="Appareils" aria-label="Rechercher par appareils"></input>${chevronDownIcon}
 </label>
 
 <label>
-<input class="top-filters_ustensiles" name="ustensiles" placeholder="Ustensiles"></input>
+<input class="top-filters_ustensiles" name="ustensiles" placeholder="Ustensiles" aria-label="Rechercher par ustensiles"></input>
 ${chevronDownIcon}
 </label>
 </div>`
